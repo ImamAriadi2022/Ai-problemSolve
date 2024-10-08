c7d27875bb9e44430d09d761b4f99b0e3cd1d1e3
Imam ariadi <gamerapin139@gmail.com>
2024-10-08 14:43:44 +0700

    add favicon

--- studyMateApp/electron/main.ts
+++ studyMateApp/electron/main.ts
@@ -6,6 +6,8 @@ import path from 'node:path'
 const require = createRequire(import.meta.url)
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
+require;
+
 // The built directory structure
 //
 // ├─┬─┬ dist
